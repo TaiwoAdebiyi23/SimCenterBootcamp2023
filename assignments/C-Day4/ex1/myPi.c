18d7384a93b8176b5c88038caa3ab18a9f8b802f
Taiwo Adebiyi <taadebi2@login3.frontera.tacc.utexas.edu>
2023-06-08 17:01:15 -0500

    git practice

--- assignments/C-Day4/ex1/myPi.c
+++ assignments/C-Day4/ex1/myPi.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
   
   int numP, procID;
 
-  // ---------the usual mpi initialization------
+  // ---------the usual mpi initialization-------------
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numP);
   MPI_Comm_rank(MPI_COMM_WORLD, &procID);
